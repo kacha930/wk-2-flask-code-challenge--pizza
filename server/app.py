3bc97fd5629194720fc7af81ed4e8ca19703c477
kacha930 <robiwere20@gmail.com>
2024-10-13 12:10:31 +0300

    delete button working correctly

--- server/app.py
+++ server/app.py
@@ -28,6 +28,51 @@ def index():
 def get_restaurants():
     restaurants = Restaurant.query.all()
     return jsonify([restaurant.to_dict() for restaurant in restaurants])
+@app.route('/restaurants/<int:id>', methods=['GET'])
+def get_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+    if restaurant:
+        return jsonify(restaurant.to_dict_with_pizza())
+    else:
+        return jsonify({'error': 'Restaurant not found'}), 404
+    
+
+@app.route('/restaurants/<int:id>', methods=['DELETE'])
+def delete_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+    if restaurant:
+        db.session.delete(restaurant)
+        db.session.commit()
+        return jsonify({'message': 'Restaurant deleted'}), 204
+    else:
+        return jsonify({'error': 'Restaurant not found'}), 404
+    
+
+@app.route('/pizzas', methods=['GET'])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return jsonify([pizza.to_dict() for pizza in pizzas])
+
+@app.route('/restaurant_pizzas', methods=['POST'])
+def create_restaurant_pizza():
+    data = request.get_json()
+    try:
+        new_rp = RestaurantPizza(
+            price=data['price'],
+            pizza_id=data['pizza_id'],
+            restaurant_id=data['restaurant_id']
+        )
+        db.session.add(new_rp)
+        db.session.commit()
+        return make_response(new_rp.to_dict_with_relationships(), 201)
+    
+    except Exception as e:
+        return make_response(jsonify({'error': str(e)})), 400
+
+
+         
+    
+
 
 
 if __name__ == '__main__':
